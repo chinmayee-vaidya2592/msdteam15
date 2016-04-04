3d23bed1356503c87d896af38d219710a5b987e9
bhumitra nagar <nagar.b@husky.neu.edu>
2016-04-04 19:00:44 -0400

    navigation drawer added to myops and mainactivity

--- MainActivity/app/src/main/java/com/example/chinmayee/mainactivity/MainActivity.java
+++ MainActivity/app/src/main/java/com/example/chinmayee/mainactivity/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.chinmayee.mainactivity;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.graphics.BitmapFactory;
 import android.graphics.Bitmap;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -32,11 +41,35 @@ public class MainActivity extends AppCompatActivity {
     ProgressBar mBar4;
     ProgressBar mBar5;
 
+    //new
+
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private String mActivityTitle;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Firebase.setAndroidContext(this);
+
+        setupToolbar();
+        mDrawerList = (ListView)findViewById(R.id.navList1);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout1);
+        mActivityTitle = "MY INFO";
+
+
+        addDrawerItems();
+        setupDrawer();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+
         Firebase mFBRef = new Firebase("https://flickering-inferno-293.firebaseio.com/");
 
         imageView1 = (ImageView) findViewById(R.id.imageView1);
@@ -77,8 +110,8 @@ public class MainActivity extends AppCompatActivity {
 
                     // Set Total progress bar and score
                     int[] dimScore = new int[5];
-                    int sumScore=0;
-                    for (int j=1; j<6; j++) {
+                    int sumScore = 0;
+                    for (int j = 1; j < 6; j++) {
                         dimScore[j - 1] = Integer.parseInt((String) postSnapshot.child("dimensions").child("d" + j).getValue());
                         sumScore += dimScore[j - 1];
                     }
@@ -100,7 +133,81 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println("The read failed: " + firebaseError.getMessage());
             }
         });
+
     }
+
+    private void setupToolbar() {
+        // TODO Auto-generated method stub
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarsdfs1);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);}
+    }
+
+    private void addDrawerItems() {
+        String[] osArray = { "HOME", "MY INFO", "MY OPPORTUNITIES", "LOGOUT" };
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+        mDrawerList.setAdapter(mAdapter);
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Toast.makeText(MyOppsActivity.this, "Time for an upgrade!"+position+" "+id, Toast.LENGTH_SHORT).show();
+                openActivity(position);
+            }
+        });
+    }
+
+    protected void openActivity(int position) {
+
+        mDrawerList.setItemChecked(position, true);
+//		setTitle(listArray[position]);
+        mDrawerLayout.closeDrawer(mDrawerList);
+        // BaseActivity.position = position; //Setting currently selected position in this field so that it will be available in our child activities.
+
+        switch (position) {
+            case 0:
+                startActivity(new Intent(this, HomeActivity.class));
+                break;
+            case 1:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case 2:
+                startActivity(new Intent(this, MyOppsActivity.class));
+                break;
+            case 3:
+                //  startActivity(new Intent(this, Item3Activity.class));
+                break;
+
+            default:
+                break;
+        }
+
+//		Toast.makeText(this, "Selected Item Position::"+position, Toast.LENGTH_LONG).show();
+    }
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("MENU");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle("MY INFO");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -121,6 +228,24 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
 
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
 }
--- MainActivity/app/src/main/java/com/example/chinmayee/mainactivity/MyOppsActivity.java
+++ MainActivity/app/src/main/java/com/example/chinmayee/mainactivity/MyOppsActivity.java
@@ -3,14 +3,40 @@ package com.example.chinmayee.mainactivity;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.firebase.client.Firebase;
 
-;
+
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 
 public class MyOppsActivity extends AppCompatActivity {
+
+
+    //new
+    private ListView mDrawerList;
+    private DrawerLayout mDrawerLayout;
+    private ArrayAdapter<String> mAdapter;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private String mActivityTitle;
+
     TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +45,127 @@ public class MyOppsActivity extends AppCompatActivity {
         Firebase.setAndroidContext(this);
         setupToolbar();
         setupTablayout();
+
+        mDrawerList = (ListView)findViewById(R.id.navList);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mActivityTitle = "MY OPPORTUNITIES";
+
+        addDrawerItems();
+        setupDrawer();
+
     }
+
+
+    //NEW METHODS
+
+    private void addDrawerItems() {
+        String[] osArray = { "HOME", "MY INFO", "MY OPPORTUNITIES", "LOGOUT" };
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+        mDrawerList.setAdapter(mAdapter);
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Toast.makeText(MyOppsActivity.this, "Time for an upgrade!"+position+" "+id, Toast.LENGTH_SHORT).show();
+                openActivity(position);
+
+
+            }
+        });
+
+    }
+
+    protected void openActivity(int position) {
+
+
+        mDrawerList.setItemChecked(position, true);
+//		setTitle(listArray[position]);
+        mDrawerLayout.closeDrawer(mDrawerList);
+        // BaseActivity.position = position; //Setting currently selected position in this field so that it will be available in our child activities.
+
+        switch (position) {
+            case 0:
+                startActivity(new Intent(this, HomeActivity.class));
+                break;
+            case 1:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case 2:
+                startActivity(new Intent(this, MyOppsActivity.class));
+                break;
+            case 3:
+                //  startActivity(new Intent(this, Item3Activity.class));
+                break;
+            case 4:
+                // startActivity(new Intent(this, Item4Activity.class));
+                break;
+            case 5:
+                // startActivity(new Intent(this, Item5Activity.class));
+                break;
+
+            default:
+                break;
+        }
+
+//		Toast.makeText(this, "Selected Item Position::"+position, Toast.LENGTH_LONG).show();
+    }
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("MENU");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    //END NEW METHODS
 
     private void setupToolbar() {
         // TODO Auto-generated method stub
